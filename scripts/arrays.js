2f85a71e869d40352026ab5faf2c8383f1ac14ac
Bethany Fannin <contact@bethanyfannin.com>
2024-03-21 14:52:21 -0400

    feat(javascript): exercise one code display
    Create code that allows the inputs and outputs from the first exercise
    to be displayed on the corresponding HTML page array-cardio.html.
    
    Used reduce function on Object.keys() to concisely iterate through the
    answer object.

--- scripts/arrays.js
+++ scripts/arrays.js
@@ -1,19 +1,20 @@
-
+const answerOne = document.querySelector('.code1');
 
 const inventors = [
-  { first: "Albert", last: "Einstein", year: 1879, passed: 1955 },
-  { first: "Isaac", last: "Newton", year: 1643, passed: 1727 },
-  { first: "Galileo", last: "Galilei", year: 1564, passed: 1642 },
-  { first: "Marie", last: "Curie", year: 1867, passed: 1934 },
-  { first: "Johannes", last: "Kepler", year: 1571, passed: 1630 },
-  { first: "Nicolaus", last: "Copernicus", year: 1473, passed: 1543 },
-  { first: "Max", last: "Planck", year: 1858, passed: 1947 },
-  { first: "Katherine", last: "Blodgett", year: 1898, passed: 1979 },
-  { first: "Ada", last: "Lovelace", year: 1815, passed: 1852 },
-  { first: "Sarah E.", last: "Goode", year: 1855, passed: 1905 },
-  { first: "Lise", last: "Meitner", year: 1878, passed: 1968 },
-  { first: "Hanna", last: "Hammarström", year: 1829, passed: 1909 },
+  { first: "Albert", last: "Einstein", year: 1879, passed: 1955 }, // 76 6
+  { first: "Isaac", last: "Newton", year: 1643, passed: 1727 }, // 84 10
+  { first: "Galileo", last: "Galilei", year: 1564, passed: 1642 }, // 78 7
+  { first: "Marie", last: "Curie", year: 1867, passed: 1934 }, // 67 4
+  { first: "Johannes", last: "Kepler", year: 1571, passed: 1630 }, // 59 3
+  { first: "Nicolaus", last: "Copernicus", year: 1473, passed: 1543 }, // 70 5
+  { first: "Max", last: "Planck", year: 1858, passed: 1947 }, // 89 11
+  { first: "Katherine", last: "Blodgett", year: 1898, passed: 1979 }, // 81 9
+  { first: "Ada", last: "Lovelace", year: 1815, passed: 1852 }, // 37 1
+  { first: "Sarah E.", last: "Goode", year: 1855, passed: 1905 }, // 50 2
+  { first: "Lise", last: "Meitner", year: 1878, passed: 1968 }, // 90 12
+  { first: "Hanna", last: "Hammarström", year: 1829, passed: 1909 }, // 80 8
 ];
+
 
 const people = [
   "Bernhard, Sandra",
@@ -61,23 +62,57 @@ const people = [
 
 // Array.prototype.filter()
 // 1. Filter the list of inventors for those who were born in the 1500's
+const fifteen = inventors.filter((inventor) => inventor.year > 1500 && inventor.year < 1599);
+
+console.log(fifteen);
+
+const generatedHtml = Object.keys(fifteen).reduce(
+  (accum, currKey) =>
+    accum +
+    `<br /> {first: ${fifteen[currKey].first},
+    <br /> last: ${fifteen[currKey].last},
+    <br /> year: ${fifteen[currKey].year},
+    <br /> passed: ${fifteen[currKey].passed}}<br /> `, '');
+
+answerOne.innerHTML = `[` + generatedHtml + `]`;
 
 // Array.prototype.map()
 // 2. Give us an array of the inventors first and last names
+const fullName = inventors.map(inventor => `${inventor.first} ${inventor.last}`)
 
 // Array.prototype.sort()
 // 3. Sort the inventors by birthdate, oldest to youngest
+const sortedByBirth = inventors.sort((a, b) => (a.year > b.year) ? 1 : (a.year < b.year) ? -1 : 0);
 
 // Array.prototype.reduce()
 // 4. How many years did all the inventors live all together?
+const sum = inventors.reduce(
+  (accumulator, currentValue) => accumulator + (currentValue.passed - currentValue.year),
+  0,
+);
 
 // 5. Sort the inventors by years lived
+const yearsLived = inventors.sort((a, b) => {
+  const firstOne = parseInt(a.passed - a.year);
+  const nextOne = parseInt(b.passed - b.year);
+  return nextOne > firstOne ? -1 : 1;
+});
+
 
 // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
 // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
 
 // 7. sort Exercise
 // Sort the people alphabetically by last name
+/*function sortPeople(...people) {
+  let peopleArr = people;
+  peopleArr.split(', ');
+  console.log(peopleArr);
+  /*peopleArr.sort((a, b) => {
+
+  })
+
+sortPeople();
 
 // 8. Reduce Exercise
 // Sum up the instances of each of these
@@ -96,4 +131,4 @@ const data = [
   "van",
   "car",
   "truck",
-];
+];*/
